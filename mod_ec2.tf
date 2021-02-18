f20c88328c97cbc37ce4b554950836b8ef9c2969
smsaar2009 <smsaar2009@users.noreply.github.com>
2021-02-18 20:30:59 +0530

    Update mod_ec2.tf

--- mod_ec2.tf
+++ mod_ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myfirstterraformeec2" {
   ami           = "ami-0db0b3ab7df22e366"
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
   key_name = "lele"
 }
